9c8e8dad9f319e28191127568969b6bf3ad87c6a
Patrick Pichon <patrick@pichon.me>
2020-09-01 14:36:09 +0200

    Creation of Lux/Motion widgets

--- plugin.py
+++ plugin.py
@@ -125,12 +125,17 @@ class JSONPersistingListener:
             if endpoint_id == 0:
                 continue  # Skip zdo
 
-            endpoint_obj = {}
-            endpoint_obj['id'] = endpoint_id
-            endpoint_obj['status'] = endpoint.status
-            endpoint_obj['device_type'] = getattr(endpoint, 'device_type', None)
-            endpoint_obj['profile_id'] = getattr(endpoint, 'profile_id', None)
-            endpoint_obj['output_clusters'] = [cluster.cluster_id for cluster in endpoint.out_clusters.values()]
+            endpoint_obj = {
+                'id': endpoint_id,
+                'status': endpoint.status,
+                'device_type': getattr(endpoint, 'device_type', None),
+                'profile_id': getattr(endpoint, 'profile_id', None),
+                'output_clusters': [
+                    cluster.cluster_id
+                    for cluster in endpoint.out_clusters.values()
+                ],
+            }
+
             endpoint_obj['input_clusters'] = [cluster.cluster_id for cluster in endpoint.in_clusters.values()]
 
             obj['endpoints'].append(endpoint_obj)
@@ -138,10 +143,7 @@ class JSONPersistingListener:
         return obj
 
     def _dump(self):
-        devices = []
-
-        for device in self._application.devices.values():
-            devices.append(self._serialize_device(device))
+        devices = [ self._serialize_device(device) for device in self._application.devices.values() ]
 
         existing = self._database_file.read_text()
         new = json.dumps({'devices': devices}, separators=(', ', ': '), indent=4)
@@ -214,9 +216,10 @@ class MainListener:
     Look for `listener_event` in the Zigpy source or just look at the logged warnings.
     """
 
-    def __init__(self, application):
-        Domoticz.Log("MainListener init")
+    def __init__(self, application, Devices):
+        Domoticz.Log("MainListener init App: %s Devices: %s" %(application, Devices))
         self.application = application
+        self.domoticzDevices = Devices
 
     def device_joined(self, device):
         Domoticz.Log(f"Device joined: {device}")
@@ -228,8 +231,15 @@ class MainListener:
         """
         Called at runtime after a device's information has been queried.
         I also call it on startup to load existing devices from the DB.
+
+        Example:
+        INFO:plugin:Device is ready: new=True, device=<zhaquirks.xiaomi.aqara.motion_aq2.MotionAQ2 object at 0xac2af1c0> 
+            signature={1: {'in_clusters': [0, 1, 3, 1024, 1030, 1280, 65535], 'out_clusters': [0, 25]}}
         """
-        LOGGER.info("Device is ready: new=%s, device=%s signature=%s", new, device, device.get_signature())
+        LOGGER.info("Device is ready: new=%s, device=%s NwkId: %s IEEE: %s signature=%s", new, device, device.nwk, device._ieee, device.get_signature())
+        
+        if device.nwk != 0x0000 and len( device.get_signature()) > 0:     
+            domoCreateDevice( self, device._ieee, device.get_signature() )
 
         for ep_id, endpoint in device.endpoints.items():
             # Ignore ZDO
@@ -245,8 +255,9 @@ class MainListener:
     def attribute_updated(self, cluster, attribute_id, value):
         # Each object is linked to its parent (i.e. app > device > endpoint > cluster)
         device = cluster.endpoint.device
-
-        Domoticz.Log("Received an attribute update %s=%s on cluster %s from device %s" %( attribute_id, value, cluster, device) )
+        Domoticz.Log("Device Signature: %s" %device.get_signature())
+        Domoticz.Log("Received an attribute update %s=%s on cluster %s from device %s/%s" %( attribute_id, value, cluster, device, device._ieee) )
+        Domoticz.Log("Cluster %04x Attribute: %s value: %s type(%s)" %(cluster.cluster_id, attribute_id, value, type(value)))
 
 
 async def main( self ):
@@ -264,11 +275,11 @@ async def main( self ):
     # Make sure that we have the quirks embedded.
     import zhaquirks  # noqa: F401
 
-    if self.DomoticzParams["Mode1"] in ( 'USB', 'DIN'):
-        path = self.DomoticzParams["SerialPort"]
+    if self.domoticzParameters["Mode1"] in ( 'USB', 'DIN'):
+        path = self.domoticzParameters["SerialPort"]
 
-    elif self.DomoticzParams["Mode1"] == 'PI':
-        path = 'pizigate:%s' %self.DomoticzParams["SerialPort"]
+    elif self.domoticzParameters["Mode1"] == 'PI':
+        path = 'pizigate:%s' %self.domoticzParameters["SerialPort"]
 
     else:
         Domoticz.Error("Mode: %s Not implemented Yet")
@@ -276,7 +287,7 @@ async def main( self ):
 
     self.zigpyApp = await JSONControllerApplication.new(
         config=ControllerApplication.SCHEMA({
-            "json_database_path": self.DomoticzParams["HomeFolder"] + PERSISTENT_DB,
+            "json_database_path": self.domoticzParameters["HomeFolder"] + PERSISTENT_DB,
             "device": {
                 "path": path, 
             }
@@ -285,7 +296,7 @@ async def main( self ):
         start_radio=True,
     )
 
-    listener = MainListener( self.zigpyApp )
+    listener = MainListener( self.zigpyApp, self.domoticzDevices )
     self.zigpyApp.add_listener(listener)
 
     # Have every device in the database fire the same event so you can attach listeners
@@ -306,7 +317,8 @@ class BasePlugin:
     def __init__(self):
         self.zigpyThread = None
         self.zigpyApp = None
-        self.DomoticzParams = {}
+        self.domoticzParameters = {}
+        self.domoticzDevices = None
         
         logging.basicConfig(level=logging.INFO)     
 
@@ -343,15 +355,17 @@ class BasePlugin:
 
     def onStart(self):
         logging.basicConfig(level=logging.DEBUG)
-        self.DomoticzParams = dict(Parameters)
+        self.domoticzParameters = dict(Parameters)
+        DumpConfigToLog()
+        self.domoticzDevices = Devices
+
         Domoticz.Log("onStart called")
         self.zigpyThread = threading.Thread(
                             name="ZigpyThread", 
                             target=BasePlugin.zigpy_thread,
                             args=(self,))
         self.zigpyThread.start()
-        
-        
+            
     def onStop(self):
         Domoticz.Log("onStop called")
         self.zigpyApp.shutdown()
@@ -373,6 +387,72 @@ class BasePlugin:
 
     def onHeartbeat(self):
         Domoticz.Log("onHeartbeat called")
+
+def domoCreateDevice( self, device_ieee, device_signature):
+
+    Domoticz.Log("device_signature: %s" %device_signature)
+    for ep in device_signature:
+        
+        Domoticz.Log(" --> ep: %s" %ep)
+        in_cluster = device_signature[ep]['in_clusters']
+        Domoticz.Log("--> IN Cluster: %s" %(in_cluster))
+        for cluster in in_cluster:
+            Domoticz.Log("----> Cluster: %s" %cluster)
+            widget_type = get_type_from_cluster( cluster )
+            Domoticz.Log("---------> Widget Type: %s" %widget_type)
+
+            if widget_type is None:
+                continue
+        
+            elif widget_type == 'Switch':
+                Domoticz.Log("----> Create Switch")
+                createDomoticzWidget( self, device_ieee, ep, widget_type, widgetType='Light/Switch')
+
+            elif widget_type == 'Lux':
+                Domoticz.Log("----> Create Lux")
+                createDomoticzWidget( self, device_ieee, ep, widget_type, widgetType='Lux')
+                
+            elif widget_type == 'Motion':
+                Domoticz.Log("----> Create Motion")
+                createDomoticzWidget( self, device_ieee, ep, widget_type, widgetType='Motion')
+ 
+
+def createDomoticzWidget( self, ieee, ep, cType, widgetType = None,
+                         Type_ = None, Subtype_ = None, Switchtype_ = None ): 
+
+    Domoticz.Log("createDomoticzWidget")
+    unit = getFreeUnit(self)
+    Domoticz.Log("--> Unit: %s" %unit)
+    widgetName = '%s %s - %s' %(widgetType, ieee, ep )
+    Domoticz.Log("--> widgetName: %s" %widgetName)
+    if widgetType:
+        Domoticz.Log("Creating device is Domoticz DeviceID:%s Name: %s Unit: %s TypeName: %s" %(ieee, widgetName, unit, widgetType))
+        myDev = Domoticz.Device( DeviceID = str(ieee), Name = widgetName, Unit = unit, TypeName = widgetType )
+        myDev.Create()
+        ID = myDev.ID
+        if myDev.ID == -1 :
+            Domoticz.Error("Domoticz widget creation failed. Check that Domoticz can Accept New Hardware [%s]" %myDev )
+
+
+def getFreeUnit(self, nbunit_=1):
+    '''
+    FreeUnit
+    Look for a Free Unit number. If nbunit > 1 then we look for nbunit consecutive slots
+    '''
+    Domoticz.Log("getFreeUnit - Devices: %s" %len(self.domoticzDevices))
+    return len(self.domoticzDevices) + 1
+
+def get_type_from_cluster( cluster ):
+    # return a Widget Type list based on the available Cluster 
+
+    TYPE_LIST = {
+        0x0006:'Switch',
+        0x0400:'Lux',
+        0x0406:'Motion'
+    }
+    if cluster not in TYPE_LIST:
+        return None
+    return TYPE_LIST[ cluster ]
 
 global _plugin
 _plugin = BasePlugin()
@@ -410,16 +490,17 @@ def onHeartbeat():
     _plugin.onHeartbeat()
 
     # Generic helper functions
+
 def DumpConfigToLog():
     for x in Parameters:
         if Parameters[x] != "":
             Domoticz.Debug( "'" + x + "':'" + str(Parameters[x]) + "'")
-    Domoticz.Debug("Device count: " + str(len(Devices)))
+    Domoticz.Log("Device count: " + str(len(Devices)))
     for x in Devices:
-        Domoticz.Debug("Device:           " + str(x) + " - " + str(Devices[x]))
-        Domoticz.Debug("Device ID:       '" + str(Devices[x].ID) + "'")
-        Domoticz.Debug("Device Name:     '" + Devices[x].Name + "'")
-        Domoticz.Debug("Device nValue:    " + str(Devices[x].nValue))
-        Domoticz.Debug("Device sValue:   '" + Devices[x].sValue + "'")
-        Domoticz.Debug("Device LastLevel: " + str(Devices[x].LastLevel))
+        Domoticz.Log("Device:           " + str(x) + " - " + str(Devices[x]))
+        Domoticz.Log("Device ID:       '" + str(Devices[x].ID) + "'")
+        Domoticz.Log("Device Name:     '" + Devices[x].Name + "'")
+        Domoticz.Log("Device nValue:    " + str(Devices[x].nValue))
+        Domoticz.Log("Device sValue:   '" + Devices[x].sValue + "'")
+        Domoticz.Log("Device LastLevel: " + str(Devices[x].LastLevel))
     return
