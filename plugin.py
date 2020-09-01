a463ea1a9f4137ba0ddd8d5984f46e9a21a98d0b
Patrick Pichon <patrick@pichon.me>
2020-09-01 22:24:10 +0200

    working with the sqlite3 persistent DB. ATTENTION required a special version of Domoticz binaries

--- plugin.py
+++ plugin.py
@@ -22,7 +22,13 @@
     Please use first the Domoticz forums in order to qualify your issue. Select the ZigBee or Zigate topic.
     </description>
     <params>
-        <param field="Mode1" label="Zigate Model" width="75px" required="true" default="None">
+        <param field="Mode1" label="HW Model" width="75px" required="true" default="None">
+            <options>
+                <option label="ZiGate" value="ZiGate" default="None" />
+            </options>
+        </param>
+
+        <param field="Mode2" label="Zigate Model" width="75px" required="true" default="None">
             <options>
                 <option label="USB" value="USB" default="true" />
                 <option label="DIN" value="DIN" />
@@ -31,6 +37,7 @@
                 <option label="None" value="None"/>
             </options>
         </param>
+
         <param field="Address" label="IP" width="150px" required="true" default="0.0.0.0"/>
         <param field="Port" label="Port" width="150px" required="true" default="9999"/>
         <param field="SerialPort" label="Serial Port" width="150px" required="true" default="/dev/ttyUSB0"/>
@@ -67,10 +74,10 @@ import threading
 import logging
 
 
-PERSISTENT_DB = 'Data/zigpy.json'
+PERSISTENT_DB = 'Data/zigpy'
 
 # There are many different radio libraries but they all have the same API
-from zigpy_zigate.zigbee.application import ControllerApplication
+
 
 
 import zigpy.config as config
@@ -81,133 +88,6 @@ import pathlib
 
 import zigpy.types
 from zigpy.zdo import types as zdo_t
-
-class JSONPersistingListener:
-    # This provides persistent storage of the zigpy Device Database.
-    def __init__(self, database_file, application):
-        self._database_file = pathlib.Path(database_file)
-        self._application = application
-
-        self._db = {'devices': {}}
-
-    def device_joined(self, device):
-        self.raw_device_initialized(device)
-
-    def device_initialized(self, device):
-        # This is passed in a quirks device
-        pass
-
-    def device_left(self, device):
-        self._db['devices'].pop(str(device.ieee))
-        self._dump()
-
-    def raw_device_initialized(self, device):
-        self._db['devices'][str(device.ieee)] = self._serialize_device(device)
-        self._dump()
-
-    def device_removed(self, device):
-        self._db['devices'].pop(str(device.ieee))
-        self._dump()
-
-    def attribute_updated(self, cluster, attrid, value):
-        self._dump()
-
-    def _serialize_device(self, device):
-        obj = {
-            'ieee': str(device.ieee),
-            'nwk': device.nwk,
-            'status': device.status,
-            'node_descriptor': None if not device.node_desc.is_valid else list(device.node_desc.serialize()),
-            'endpoints': [],
-        }
-
-        for endpoint_id, endpoint in device.endpoints.items():
-            if endpoint_id == 0:
-                continue  # Skip zdo
-
-            endpoint_obj = {
-                'id': endpoint_id,
-                'status': endpoint.status,
-                'device_type': getattr(endpoint, 'device_type', None),
-                'profile_id': getattr(endpoint, 'profile_id', None),
-                'output_clusters': [
-                    cluster.cluster_id
-                    for cluster in endpoint.out_clusters.values()
-                ],
-            }
-
-            endpoint_obj['input_clusters'] = [cluster.cluster_id for cluster in endpoint.in_clusters.values()]
-
-            obj['endpoints'].append(endpoint_obj)
-
-        return obj
-
-    def _dump(self):
-        devices = [ self._serialize_device(device) for device in self._application.devices.values() ]
-
-        existing = self._database_file.read_text()
-        new = json.dumps({'devices': devices}, separators=(', ', ': '), indent=4)
-
-        # Don't waste writes
-        if existing == new:
-            return
-
-        self._database_file.write_text(new)
-
-    def load(self):
-        try:
-            state_obj = json.loads(self._database_file.read_text())
-        except FileNotFoundError:
-            Domoticz.Error('Database is empty! Creating an empty one...')
-            self._database_file.write_text('')
-
-            state_obj = {'devices': []}
-
-        for obj in state_obj['devices']:
-            ieee = zigpy.types.named.EUI64([zigpy.types.uint8_t(int(c, 16)) for c in obj['ieee'].split(':')][::-1])
-
-            assert obj['ieee'] in repr(ieee)
-
-            device = self._application.add_device(ieee, obj['nwk'])
-            device.status = zigpy.device.Status(obj['status'])
-
-            if 'node_descriptor' in obj and obj['node_descriptor'] is not None:
-                device.node_desc = zdo_t.NodeDescriptor.deserialize(bytearray(obj['node_descriptor']))[0]
-
-            for endpoint_obj in obj['endpoints']:
-                endpoint = device.add_endpoint(endpoint_obj['id'])
-                endpoint.profile_id = endpoint_obj['profile_id']
-                device_type = endpoint_obj['device_type']
-
-                try:
-                    if endpoint.profile_id == 260:
-                        device_type = zigpy.profiles.zha.DeviceType(device_type)
-                    elif endpoint.profile_id == 49246:
-                        device_type = zigpy.profiles.zll.DeviceType(device_type)
-                except ValueError:
-                    pass
-
-                endpoint.device_type = device_type
-                endpoint.status = zigpy.endpoint.Status(endpoint_obj['status'])
-
-                for output_cluster in endpoint_obj['output_clusters']:
-                    endpoint.add_output_cluster(output_cluster)
-
-                for input_cluster in endpoint_obj['input_clusters']:
-                    cluster = endpoint.add_input_cluster(input_cluster)
-
-
-# Use this in place of your radio's ControllerApplication
-class JSONControllerApplication(ControllerApplication):
-    def __init__(self, config):
-        super().__init__(self.SCHEMA(config))
-
-        # Makes the Zigpy database persistent.
-        self._dblistener = JSONPersistingListener(self.config[ 'json_database_path' ], self)
-        
-        self.add_listener(self._dblistener)
-        self._dblistener.load()
-        self._dblistener._dump()
 
 
 class MainListener:
@@ -261,40 +141,39 @@ class MainListener:
         domoMajDevice( self, device._ieee, cluster.cluster_id, attribute_id, value )
 
 async def main( self ):
-    #self.zigpyApp = await ControllerApplication.new(
-    #    config=ControllerApplication.SCHEMA({
-    #        "database_path": "/tmp/zigpy.db",
-    ##        "device": {
-    #            "path": "/dev/null", #/dev/ttyUSBRPI3",
-    #        }
-    #    }),
-    ###    auto_form=True,
-    #    start_radio=True,
-    #)
-
+                                       
     # Make sure that we have the quirks embedded.
     import zhaquirks  # noqa: F401
 
-    if self.domoticzParameters["Mode1"] in ( 'USB', 'DIN'):
-        path = self.domoticzParameters["SerialPort"]
+    # Instantiate with the corresponding Zigbee Radio
+    if self.domoticzParameters["Mode1"] == 'ZiGate':
+        if self.domoticzParameters["Mode2"] in ( 'USB', 'DIN'):
+            path = self.domoticzParameters["SerialPort"]
 
-    elif self.domoticzParameters["Mode1"] == 'PI':
-        path = 'pizigate:%s' %self.domoticzParameters["SerialPort"]
+        elif self.domoticzParameters["Mode2"] == 'PI':
+            path = 'pizigate:%s' %self.domoticzParameters["SerialPort"]
+
+        else:
+            Domoticz.Error("Mode: %s Not implemented Yet" %self.domoticzParameters["Mode2"])
+            return
+
+        from zigpy_zigate.zigbee.application import ControllerApplication
+        self.zigpyApp = await ControllerApplication.new(
+            config=ControllerApplication.SCHEMA({
+                "database_path": self.domoticzParameters["HomeFolder"] + PERSISTENT_DB + '.db',
+                "device": {
+                    "path": path,
+                }
+            }),
+            auto_form=True,
+            start_radio=True,
+        )
 
     else:
-        Domoticz.Error("Mode: %s Not implemented Yet")
+        Domoticz.Error("Mode: %s Not implemented Yet" %self.domoticzParameters["Mode1"])
         return
 
-    self.zigpyApp = await JSONControllerApplication.new(
-        config=ControllerApplication.SCHEMA({
-            "json_database_path": self.domoticzParameters["HomeFolder"] + PERSISTENT_DB,
-            "device": {
-                "path": path, 
-            }
-        }),
-        auto_form=True,
-        start_radio=True,
-    )
+
 
     listener = MainListener( self.zigpyApp, self.domoticzDevices )
     self.zigpyApp.add_listener(listener)
